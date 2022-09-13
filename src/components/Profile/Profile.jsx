2b32f8e9a4677a70bf49a089bb223b3c0f766e01
Serhii Kaushnian <skaushnjan@gmail.com>
2022-09-13 10:19:04 +0200

    updated propTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -42,7 +42,7 @@ export const Profile = ({ avatar, username, tag, location, stats }) => {
 };
 
 Profile.propTypes = {
-  username: PropTypes.string,
+  username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -24,6 +24,7 @@ export const Statistics = ({ data, title }) => {
 };
 
 Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
   data: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
